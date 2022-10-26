933dd9eb329d597a9c29a5e0476d01c079ad757f
Yuri Cicconi <yuricbianchii@gmail.com>
2022-10-26 10:33:27 -0300

    Exercíos de treino de comentários/documentação

---
+++ Comentarios/src/Cometarios.java
@@ -0,0 +1,22 @@
+public class Cometarios {
+    public static void main(String[] args) throws Exception {
+        
+        
+    }
+
+    /**
+     * 
+     * Com 2 asteriscos (*) no começo, o comentário se torna uma documentação do método
+     */
+
+     public void metodo() {
+
+     }
+
+     /**
+     * Para funcionar, o comentário tem que vir ANTES do método
+     */
+    public void metodo2() {
+        
+     }
+}
